302100dfb36da4b5e47d66d3af51744888c8e33f
Gaodafeiyan <candicexfx18@gmail.com>
2025-07-22 01:51:33 +0800

    快速修复：移除双向关系配置，使用单向关系让服务先启动

--- src/api/yaoqing-jiangli/content-types/yaoqing-jiangli/schema.ts
+++ src/api/yaoqing-jiangli/content-types/yaoqing-jiangli/schema.ts
@@ -6,9 +6,9 @@ const JiangliSchema = {
   attributes: {
     shouyiUSDT : { type: 'decimal', required: true },
     tuijianRen : { type: 'relation', relation: 'manyToOne',
-                   target: 'plugin::users-permissions.user', inversedBy: 'tuijianJiangli' },
+                   target: 'plugin::users-permissions.user' },
     laiyuanRen : { type: 'relation', relation: 'manyToOne',
-                   target: 'plugin::users-permissions.user', inversedBy: 'laiyuanJiangli' },
+                   target: 'plugin::users-permissions.user' },
     laiyuanDan : { type: 'relation', relation: 'oneToOne',
                    target: 'api::dinggou-dingdan.dinggou-dingdan', mappedBy: 'jiangli' },
   },
