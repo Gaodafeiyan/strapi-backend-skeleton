f65bea9437ed2211dfd886b70ec13b4d3d544711
Gaodafeiyan <candicexfx18@gmail.com>
2025-07-22 02:09:11 +0800

    完善路由配置：为dinggou-dingdan添加完整CRUD路由，删除不完整的lottery-spin模块

--- src/api/dinggou-dingdan/routes/dinggou-dingdan.ts
+++ src/api/dinggou-dingdan/routes/dinggou-dingdan.ts
@@ -1,7 +1,35 @@
 export default {
   type: 'content-api',
   routes: [
-    { method: 'POST', path: '/dinggou-dingdans', handler: 'dinggou-dingdan.createWithPlan' },
-    { method: 'POST', path: '/dinggou-dingdans/:id/redeem', handler: 'dinggou-dingdan.redeem' },
+    {
+      method: 'GET',
+      path: '/dinggou-dingdans',
+      handler: 'dinggou-dingdan.find',
+    },
+    {
+      method: 'GET',
+      path: '/dinggou-dingdans/:id',
+      handler: 'dinggou-dingdan.findOne',
+    },
+    {
+      method: 'POST',
+      path: '/dinggou-dingdans',
+      handler: 'dinggou-dingdan.createWithPlan',
+    },
+    {
+      method: 'PUT',
+      path: '/dinggou-dingdans/:id',
+      handler: 'dinggou-dingdan.update',
+    },
+    {
+      method: 'DELETE',
+      path: '/dinggou-dingdans/:id',
+      handler: 'dinggou-dingdan.delete',
+    },
+    {
+      method: 'POST',
+      path: '/dinggou-dingdans/:id/redeem',
+      handler: 'dinggou-dingdan.redeem',
+    },
   ],
 }; 
