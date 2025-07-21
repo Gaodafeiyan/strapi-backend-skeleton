ec263d4a1fc7734f827fbd8521fe2daabf6f9afc
Gaodafeiyan <candicexfx18@gmail.com>
2025-07-22 02:31:02 +0800

    启用REST插件并设置API前缀

--- config/api.ts
+++ config/api.ts
@@ -1,7 +1,9 @@
-export default {
+export default () => ({
   rest: {
+    enabled: true,      // ← 必须 true；或把 rest 整段删掉让 Strapi 默认开启
+    prefix: '/api',
     defaultLimit: 25,
     maxLimit: 100,
     withCount: true,
   },
-};
+});
