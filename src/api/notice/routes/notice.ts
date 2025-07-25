6daf65a710e183374d78b66310d829d43230da3d
Gaodafeiyan <candicexfx18@gmail.com>
2025-07-25 18:26:49 +0800

    修复Notice API路由配置，解决404错误问题

--- src/api/notice/routes/notice.ts
+++ src/api/notice/routes/notice.ts
@@ -1,1 +1,30 @@
- 
+export default {
+  type: 'content-api',
+  routes: [
+    {
+      method: 'GET',
+      path: '/api/notices',
+      handler: 'notice.find',
+    },
+    {
+      method: 'GET',
+      path: '/api/notices/:id',
+      handler: 'notice.findOne',
+    },
+    {
+      method: 'POST',
+      path: '/api/notices',
+      handler: 'notice.create',
+    },
+    {
+      method: 'PUT',
+      path: '/api/notices/:id',
+      handler: 'notice.update',
+    },
+    {
+      method: 'DELETE',
+      path: '/api/notices/:id',
+      handler: 'notice.delete',
+    },
+  ],
+}; 
